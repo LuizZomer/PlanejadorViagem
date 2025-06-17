64edf38b8724802fc302f05cc16d2a51cfb2aad2
willianminatto <willianminatto27@gmail.com>
2025-06-17 18:44:56 -0300

    fix: calender

--- frontend/src/screen/ChooseCity/index.tsx
+++ frontend/src/screen/ChooseCity/index.tsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Text, Button, Input, Icon } from "@rneui/themed";
-import { View, StyleSheet, ScrollView } from "react-native";
-import { useNavigation, NavigationProp } from "@react-navigation/native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { TRootStackParamList } from "../../routes/AppStack";
 import { OptionContainer } from "../../shared/components/OptionsButton";
 import { findPlaceByCity } from "../../services/city/find-place-by-city";
-import { TRootStackParamList } from "../../routes/AppStack";
 import { FormContainer } from "../../styles/GlobalStyles";
-import * as S from "./styles";
 
 const schema = z.object({
-  country: z.string().min(1, "Campo obrigatório"),
-  destination: z.string().min(1, "Campo obrigatório"),
-  spendingLevel: z.enum(["Muito", "Medio", "pouco"], {
-    required_error: "Campo obrigatório",
-  }),
-  startDate: z.string().min(1, "Campo obrigatório"),
-  endDate: z.string().min(1, "Campo obrigatório"),
+  country: z.string().min(1),
+  destination: z.string().min(1),
+  spendingLevel: z.enum(["Muito", "Medio", "pouco"]),
+  startDate: z.string().min(1),
+  endDate: z.string().min(1),
   hosting: z.boolean(),
 });
 
@@ -26,6 +31,8 @@ type TFormData = z.infer<typeof schema>;
 
 export const ChooseCity = () => {
   const { navigate } = useNavigation<NavigationProp<TRootStackParamList>>();
+  const [isStartDatePickerVisible, setStartDatePickerVisible] = useState(false);
+  const [isEndDatePickerVisible, setEndDatePickerVisible] = useState(false);
 
   const {
     control,
@@ -45,142 +52,155 @@ export const ChooseCity = () => {
   });
 
   const handleSend = async (data: TFormData) => {
-    const { country, destination, spendingLevel, startDate, endDate, hosting } =
-      data;
-    const cities = await findPlaceByCity({
-      destination,
-      country,
-      spendingLevel,
-      startDate,
-      endDate,
-      hosting,
-    });
+    const cities = await findPlaceByCity(data);
     navigate("PlaceList", cities);
   };
 
+  const formatDate = (date: Date) => date.toISOString().split("T")[0];
+
   return (
-    <ScrollView style={styles.container}>
-      <Text h4 style={styles.title}>
-        Crie seu planejamento
-      </Text>
-
-      <FormContainer style={styles.form}>
-        {/* Campo: Destino */}
-        <Controller
-          name="destination"
-          control={control}
-          render={({ field }) => (
-            <Input
-              label="Destino"
-              value={field.value}
-              onChangeText={field.onChange}
-              placeholder="ex: Rio de Janeiro"
-              placeholderTextColor="#9A9A9A"
-              leftIcon={
-                <Icon name="location-city" type="material" color="#00BFFF" />
-              }
-              errorMessage={errors.destination?.message}
-              inputStyle={styles.inputText}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputWrapper}
-            />
-          )}
-        />
-        {/* Campo: País */}
-        <Controller
-          name="country"
-          control={control}
-          render={({ field }) => (
-            <Input
-              label="Nome do país"
-              value={field.value}
-              onChangeText={field.onChange}
-              placeholder="ex: Brasil"
-              placeholderTextColor="#9A9A9A"
-              leftIcon={<Icon name="public" type="material" color="#00BFFF" />}
-              errorMessage={errors.country?.message}
-              inputStyle={styles.inputText}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputWrapper}
-            />
-          )}
-        />
-
-        {/* Campo: Nível de Gasto */}
-        <OptionContainer
-          title="Nível de Gasto"
-          options={[
-            { id: "pouco", label: "Pouco" },
-            { id: "Medio", label: "Médio" },
-            { id: "Muito", label: "Muito" },
-          ]}
-          defaultValue="Medio"
-          onChange={(selected) =>
-            setValue("spendingLevel", selected as "Muito" | "Medio" | "pouco")
-          }
-          error={errors.spendingLevel?.message}
-        />
-
-        {/* Campo: Data de Início */}
-        <Controller
-          name="startDate"
-          control={control}
-          render={({ field }) => (
-            <Input
-              label="Data de Início"
-              value={field.value}
-              onChangeText={field.onChange}
-              placeholder="ex: 2025-05-12"
-              placeholderTextColor="#9A9A9A"
-              leftIcon={
-                <Icon name="calendar-today" type="material" color="#00BFFF" />
-              }
-              errorMessage={errors.startDate?.message}
-              inputStyle={styles.inputText}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputWrapper}
-            />
-          )}
-        />
-
-        {/* Campo: Data de Fim */}
-        <Controller
-          name="endDate"
-          control={control}
-          render={({ field }) => (
-            <Input
-              label="Data de Fim"
-              value={field.value}
-              onChangeText={field.onChange}
-              placeholder="ex: 2025-05-15"
-              placeholderTextColor="#9A9A9A"
-              leftIcon={<Icon name="event" type="material" color="#00BFFF" />}
-              errorMessage={errors.endDate?.message}
-              inputStyle={styles.inputText}
-              inputContainerStyle={styles.inputContainer}
-              containerStyle={styles.inputWrapper}
-            />
-          )}
-        />
-
-        {/* Campo: Hospedagem */}
-        <Controller
-          name="hosting"
-          control={control}
-          render={({ field }) => (
-            <OptionContainer
-              title="Precisa de Hospedagem?"
-              options={[
-                { id: "true", label: "Sim" },
-                { id: "false", label: "Não" },
-              ]}
-              defaultValue="false"
-              onChange={(selected) => setValue("hosting", selected === "true")}
-              error={errors.hosting?.message}
-            />
-          )}
-        />
-
+    <KeyboardAvoidingView
+      style={styles.flex}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0}
+    >
+      <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
+        <Text h4 style={styles.title}>
+          Crie seu planejamento
+        </Text>
+        <FormContainer style={styles.form}>
+          <Controller
+            name="destination"
+            control={control}
+            render={({ field }) => (
+              <Input
+                label="Destino"
+                value={field.value}
+                onChangeText={field.onChange}
+                placeholder="ex: Rio de Janeiro"
+                leftIcon={<Icon name="location-city" type="material" color="#00BFFF" />}
+                errorMessage={errors.destination?.message}
+                inputStyle={styles.inputText}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputWrapper}
+              />
+            )}
+          />
+          <Controller
+            name="country"
+            control={control}
+            render={({ field }) => (
+              <Input
+                label="Nome do país"
+                value={field.value}
+                onChangeText={field.onChange}
+                placeholder="ex: Brasil"
+                leftIcon={<Icon name="public" type="material" color="#00BFFF" />}
+                errorMessage={errors.country?.message}
+                inputStyle={styles.inputText}
+                inputContainerStyle={styles.inputContainer}
+                containerStyle={styles.inputWrapper}
+              />
+            )}
+          />
+          <OptionContainer
+            title="Nível de Gasto"
+            options={[
+              { id: "pouco", label: "Pouco" },
+              { id: "Medio", label: "Médio" },
+              { id: "Muito", label: "Muito" },
+            ]}
+            defaultValue="Medio"
+            onChange={(selected) =>
+              setValue("spendingLevel", selected as "Muito" | "Medio" | "pouco")
+            }
+            error={errors.spendingLevel?.message}
+          />
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <>
+                <Input
+                  label="Data de Início"
+                  value={field.value}
+                  onChangeText={field.onChange}
+                  placeholder="ex: 2025-06-17"
+                  onFocus={() => setStartDatePickerVisible(true)}
+                  errorMessage={errors.startDate?.message}
+                  inputStyle={styles.inputText}
+                  inputContainerStyle={styles.inputContainer}
+                  containerStyle={styles.inputWrapper}
+                  leftIcon={
+                    <TouchableOpacity onPress={() => setStartDatePickerVisible(true)}>
+                      <Icon name="calendar-today" type="material" color="#00BFFF" />
+                    </TouchableOpacity>
+                  }
+                />
+                <DateTimePickerModal
+                  isVisible={isStartDatePickerVisible}
+                  mode="date"
+                  onConfirm={(date) => {
+                    setValue("startDate", formatDate(date), { shouldValidate: true });
+                    setStartDatePickerVisible(false);
+                  }}
+                  onCancel={() => setStartDatePickerVisible(false)}
+                />
+              </>
+            )}
+          />
+          <Controller
+            name="endDate"
+            control={control}
+            render={({ field }) => (
+              <>
+                <Input
+                  label="Data de Fim"
+                  value={field.value}
+                  onChangeText={field.onChange}
+                  placeholder="ex: 2025-06-20"
+                  onFocus={() => setEndDatePickerVisible(true)}
+                  errorMessage={errors.endDate?.message}
+                  inputStyle={styles.inputText}
+                  inputContainerStyle={styles.inputContainer}
+                  containerStyle={styles.inputWrapper}
+                  leftIcon={
+                    <TouchableOpacity onPress={() => setEndDatePickerVisible(true)}>
+                      <Icon name="event" type="material" color="#00BFFF" />
+                    </TouchableOpacity>
+                  }
+                />
+                <DateTimePickerModal
+                  isVisible={isEndDatePickerVisible}
+                  mode="date"
+                  onConfirm={(date) => {
+                    setValue("endDate", formatDate(date), { shouldValidate: true });
+                    setEndDatePickerVisible(false);
+                  }}
+                  onCancel={() => setEndDatePickerVisible(false)}
+                />
+              </>
+            )}
+          />
+          <Controller
+            name="hosting"
+            control={control}
+            render={({ field }) => (
+              <OptionContainer
+                title="Precisa de Hospedagem?"
+                options={[
+                  { id: "true", label: "Sim" },
+                  { id: "false", label: "Não" },
+                ]}
+                defaultValue="false"
+                onChange={(selected) => setValue("hosting", selected === "true")}
+                error={errors.hosting?.message}
+              />
+            )}
+          />
+        </FormContainer>
+      </ScrollView>
+      <View style={styles.buttonWrapper}>
         <Button
           onPress={handleSubmit(handleSend)}
           disabled={isSubmitting}
@@ -189,17 +209,19 @@ export const ChooseCity = () => {
         >
           {isSubmitting ? "Enviando..." : "ENVIAR"}
         </Button>
-      </FormContainer>
-    </ScrollView>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  flex: {
     flex: 1,
-    backgroundColor: "#F5F9FF",
+  },
+  container: {
     paddingHorizontal: 24,
     paddingTop: 40,
+    paddingBottom: 20,
   },
   title: {
     textAlign: "center",
@@ -232,11 +254,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 6,
   },
+  buttonWrapper: {
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    backgroundColor: "#F5F9FF",
+  },
   button: {
     backgroundColor: "#00BFFF",
     borderRadius: 10,
     paddingVertical: 14,
-    marginTop: 20,
   },
   buttonText: {
     fontWeight: "bold",
